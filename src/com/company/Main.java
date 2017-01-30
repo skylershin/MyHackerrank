64d4b5f827b0852502a33973efd5ea96c44d7616
MunkyuShin <munkyu.s.shin@gmail.com>
2017-01-31 00:26:51 +0900

    quick sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,58 +1,81 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
+    static ArrayList<Integer> quickSort(int[] ar) {
+        ArrayList<Integer> left = new ArrayList();
+        ArrayList<Integer> equal = new ArrayList();
+        ArrayList<Integer> right = new ArrayList();
 
-    static class Node {
-        int data;
-        Node next;
+        partition(left, equal, right, ar);
+
+        int leftSize = left.size();
+        int rightSize = right.size();
+
+        if (leftSize > 1) {
+            left = quickSort(array(left));
+        }
+        if (rightSize > 1) {
+            right = quickSort(array(right));
+        }
+
+        return merge(left, equal, right);
     }
 
-    public static Node reverse(Node head) {
-        if (head == null) {
-            return head;
-        }
-        Node current = head;
-        Stack<Node> stack = new Stack();
-        while (current != null) {
-            stack.push(current);
-            current = current.next;
-        }
+    static void partition(ArrayList<Integer> left,
+                          ArrayList<Integer> equal,
+                          ArrayList<Integer> right,
+                          int[] ar)  {
+        int pivot = ar[0];
 
-        Node first = stack.pop();
-        Node before = first;
-        while (!stack.empty()) {
-            Node next = stack.pop();
-            next.next = null;
-            before.next = next;
-
-            before = next;
+        for (int element: ar) {
+            if (element < pivot) {
+                left.add(element);
+            } else if (element == pivot) {
+                equal.add(element);
+            } else if (element > pivot) {
+                right.add(element);
+            }
         }
+    }
 
-        return first;
+    static ArrayList<Integer> merge(ArrayList<Integer> left,
+                                    ArrayList<Integer> equal,
+                                    ArrayList<Integer> right) {
+        ArrayList resultList = new ArrayList();
+        resultList.addAll(left);
+        resultList.addAll(equal);
+        resultList.addAll(right);
+        printArray(array(resultList));
+
+        return resultList;
+    }
+
+    static int[] array(ArrayList<Integer> list) {
+        int[] result = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 
 
-    /* Tail starts here */
+    static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
-        Node head = new Node();
-        head.data = 1;
-        Node second = new Node();
-        second.data = 2;
-        head.next = second;
-
-        printNode(reverse(head));
-    }
-
-    public static void printNode(Node head) {
-        Node current = head;
-        while (current != null) {
-            System.out.println(current.data);
-            current = current.next;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+            ar[i]=in.nextInt();
         }
+        quickSort(ar);
     }
 }
